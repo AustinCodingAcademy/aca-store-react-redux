81d7044b81df91774e98518cb924d7bb6f081b1b
uhuru3 <uhuru3000@gmail.com>
2019-03-05 12:44:57 -0600

    changes made

--- public/js/components/ShoppingCart.js
+++ public/js/components/ShoppingCart.js
@@ -1,10 +1,23 @@
-function ShoppingCart(props){
-    const items =props.cart.map((p,i)=>{
-        return <ProductDetail key={i} product={p} />
-    });
-    return (
-      <div>
-          {items}
-      </div>
-    )
+class ShoppingCart extends React.Component {
+
+    state={
+        shoppingCart:[]
+    }
+
+    componentDidMount(){
+        let cart = store.getState().shoppingCart
+        this.setState({shoppingCart:cart})
+    }
+
+    render()
+    {
+        const items =this.state.shoppingCart.map((p,i)=>{
+            return <ProductDetail key={i} product={p} />
+        });
+        return (
+        <div>
+            {items}
+        </div>
+        )
+    }
 }
