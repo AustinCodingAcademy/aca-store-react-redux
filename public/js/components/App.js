309bb816719c45aae5e96558c9c8ea9bd9aa3f34
Abby Gottlich <abby.gottlich@gmail.com>
2019-03-05 17:15:53 -0600

    everything back to normal!

--- public/js/components/App.js
+++ public/js/components/App.js
@@ -1,48 +1,49 @@
 class App extends React.Component {
+
     state = {
-        shoppingCart: [],
         whatToShow: 0
     }
-    //  componentDidMount(){
-    //   fetch('https://acastore.herokuapp.com/products')
-    //   .then((response)=> {
-    //     return response.json();
-    //   })
-    //   .then((data) =>{
 
-    //   });
-    //  }
+    componentDidMount() {
+        // fetch('https://acastore.herokuapp.com/products')
+        //     .then((response) => {
+        //         return response.json();
+        //     })
+        //     .then((data) => {
+
+        //     });
+    }
+
     changeView = (view) => {
         // this.setState({whatToShow:view});
         this.setState(() => {
             return { whatToShow: view }
         })
     }
-    addItemToCart = (product) => {
-        this.setState((state, props) => {
-            this.state.shoppingCart.push(product);
-            return { shoppingCart: this.state.shoppingCart }
-        })
-    }
+    // addItemToCart = (product) => {
+    //     this.setState((state, props) => {
+    //         this.state.shoppingCart.push(product);
+    //         return { shoppingCart: this.state.shoppingCart }
+    //     })
+    // }
     render() {
         let content = null;
 
         if (this.state.whatToShow === 0) {
             content = <ProductList />
         } else {
-            content = <ShoppingCart cart={this.state.shoppingCart} />
+            content = <ShoppingCart />
         }
         return (
             <Layout
 
-                changeView={this.changeView}
-                shoppingCart={this.state.shoppingCart} >
+                changeView={this.changeView} >
                 {content}
             </Layout>
         );
     }
 }
 
-App.propTypes = {
-    products: PropTypes.string
-};
+// App.propTypes = {
+//     products: PropTypes.string
+// };
--- public/js/components/ShoppingCart.js
+++ public/js/components/ShoppingCart.js
@@ -1,10 +1,21 @@
-function ShoppingCart(props){
-    const items =props.cart.map((p,i)=>{
-        return <ProductDetail key={i} product={p} />
-    });
-    return (
-      <div>
-          {items}
-      </div>
-    )
+class ShoppingCart extends React.Component {
+
+    state = { shoppingCart: [] }
+
+    componentDidMount() {
+        let updatedShoppingCart = store.getState().shoppingCart;
+        this.setState({ shoppingCart: updatedShoppingCart })
+    }
+
+    render() {
+        const items = this.state.shoppingCart.map((p, i) => {
+            return <ProductDetail key={i} product={p} />
+        });
+        return (
+            <div>
+                {items}
+            </div>
+        )
+    }
+
 }
